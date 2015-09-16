532160cdd3366fa3071f9ec0f55356f04f03fcab
lb08abu <belcher_lewis@yahoo.com>
2015-09-16 16:15:52 +0200

    Added some initial code

---
+++ splines.py
@@ -0,0 +1,34 @@
+"""
+Class to represent splines for 2D curve design.
+
+To design a Spline we need:
+ - Control points
+ - Knots
+ - Coefficients (one for each control point)
+
+Calculate all basis functions N^k_i(u)
+"""
+
+
+import numpy as np
+import scipy as sp
+
+
+class Splines(object):
+
+    def __init__(self, grid, dvalues):
+        pass
+
+    def __call__(self, u, *args, **kwargs):
+        """
+
+
+        :param u:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        pass
+
+    def plot(self):
+        pass
