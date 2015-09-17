414317336e40940b1524c3daa62d328bf912727c
Angelos <atp10ato@student.lu.se>
2015-09-17 21:54:40 +0200

    I have implemented so that I can create basis components

--- splines.py
+++ splines.py
@@ -15,20 +15,121 @@ import scipy as sp
 
 
 class Splines(object):
+	#degree 	= 3
 
-    def __init__(self, grid, dvalues):
-        pass
+	def __init__(self, grid, dvalues): # grid = u? se slide 11
+		self.degree 	= 3
+		self.nbr_ds 	= len(dvalues)
+		self.u_max 	= self.degree + self.nbr_ds # + 1?
+		self.us 	= np.arange(self.u_max + 1) # i st. u?
 
-    def __call__(self, u, *args, **kwargs):
-        """
+
+    	def __call__(self, u, *args, **kwargs):
+		"""
 
 
-        :param u:
-        :param args:
-        :param kwargs:
-        :return:
-        """
-        pass
+		:param u:
+		:param args:
+		:param kwargs:
+		:return:
+		"""
+		pass
 
-    def plot(self):
-        pass
+	def plot(self):
+		pass
+
+	def N0(self, i, x):
+		return (i <= x) * (x < i + 1);
+
+	def N(self, i, x, n):
+		if n == 0:
+			return self.N0(i, x)
+		else: 
+			us = self.us
+			c1 = (x - us[i]) / (us[i + n] - us[i])
+			c2 = (us[i + n + 1] - x) / (us[i + n + 1] - us[i + 1])
+			return c1 * self.N(i, x, n - 1) + c2 * self.N(i + 1, x, n - 1)
+
+	def N2(self, i, x, n, memo): # memoized, returns new dictionaries all the time
+		if n == 0:
+			if (i, 0) not in memo:
+				memo[(i, 0)] = self.N0(i,x)
+			return memo
+		else: 
+			us = self.us
+			c1 = (x - us[i]) / (us[i + n] - us[i])
+			c2 = (us[i + n + 1] - x) / (us[i + n + 1] - us[i + 1])
+
+			if (i, n - 1) not in memo:
+				memo = self.N2(i, x, n - 1, memo)
+				#memo[(i, n - 1)] = self.N2(i, x, n - 1, memo)
+			#memo[(i + 1, n - 1)] = self.N2(i + 1, x, n - 1, memo)
+			memo 		= self.N2(i + 1, x, n - 1, memo)
+			memo[(i, n)] 	= c1 * memo[(i, n - 1)] + c2 * memo[(i + 1, n - 1)]
+
+			return memo
+
+	def N3(self, i, x, n, memo): # memoized, utilizes that an dictonary is mutable like a C pointer
+		if n == 0:
+			if (i, 0) not in memo:
+				memo[(i, 0)] = self.N0(i,x)
+			return 
+		else: 
+			if (i, n) in memo:
+				return
+
+			us = self.us
+			c1 = (x - us[i]) / (us[i + n] - us[i])
+			c2 = (us[i + n + 1] - x) / (us[i + n + 1] - us[i + 1])
+
+			if (i, n - 1) not in memo:
+				self.N3(i, x, n - 1, memo)
+			
+			self.N3(i + 1, x, n - 1, memo)
+			memo[(i, n)] = c1 * memo[(i, n - 1)] + c2 * memo[(i + 1, n - 1)]
+			return 
+
+
+
+"""
+	def d(self, x, u, d):
+
+		k itererar degree
+		n = degree
+		i = vilken knut d.v.s vilket u
+
+		dk = d;
+		for k in (np.arange(degree) + 1)
+		ak,i = (x - u[i]) / (u[i + degree + 1 - k] - u[i];
+		dk_i = (1 - a_ki) * dk_i1 + dk_i1 * dk_i
+"""
+
+def memoize(f):
+	memo = {}
+	def helper(x):
+		if x not in memo:            
+		    memo[x] = f(x)
+		return memo[x]
+	return helper
+
+close("all")
+s = Splines(np.arange(5), np.arange(5))
+x = linspace(0,5,100)
+plot(x, s.N(0,x,3))
+plot(x, s.N2(0,x,3,{})[(0,3)])
+memo = {}
+s.N3(0,x,3,memo)
+plot(x, memo[(0,3)])
+"""
+Test shows that:
+N much slower than N2
+a = np.arange(10)
+s = splines(a,a)
+%timeit t1 = s.N(0, x, 10) # 49.7 ms
+%timeit t2 = s.N2(0, x, 10, {})[(0,10)] #2.24 ms
+t3 = {}
+%timeit s.N3(0, x, 10, t3) # 954 ns!
+
+all(t1 == t2) * all(t1 == t3)
+
+"""
