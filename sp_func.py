6e2f5f7ae9be74c65fc5d4af5812e1ef682fc16c
sydman <simonydman@gmail.com>
2015-09-22 10:01:22 +0200

    updated memoization and 0/0 handling

--- sp_func.py
+++ sp_func.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python3
 
 import numpy as np
-from functools import lru_cache
+import matplotlib.pyplot as plt
+import collections
+import functools
+
+
+class memoized(object):
+   '''Decorator. Caches a function's return value each time it is called.
+   If called later with the same arguments, the cached value is returned
+   (not reevaluated).
+   '''
+   def __init__(self, func):
+      self.func = func
+      self.cache = {}
+   def __call__(self, *args):
+      if not isinstance(args, collections.Hashable):
+         # uncacheable. a list, for instance.
+         # better to not cache than blow up.
+         return self.func(*args)
+      if args in self.cache:
+         return self.cache[args]
+      else:
+         value = self.func(*args)
+         self.cache[args] = value
+         return value
+   def __repr__(self):
+      '''Return the function's docstring.'''
+      return self.func.__doc__
+   def __get__(self, obj, objtype):
+      '''Support instance methods.'''
+      return functools.partial(self.__call__, obj)
 
 
 def get_basis_func(knot_seq, j, n=3, max_cache=16):
@@ -17,16 +46,19 @@ def get_basis_func(knot_seq, j, n=3, max_cache=16):
 
     """
     
-    @lru_cache(maxsize=max_cache)
+    @memoized
     def N(x, n, i):
         if n == 0:
             return (knot_seq[i] <= x) * (x < knot_seq[i+1])
 
-        c1 = (x - knot_seq[i]) / \
-             (knot_seq[i + n] - knot_seq[i])
+        c1, c2 = 0, 0
+        if (knot_seq[i + n] - knot_seq[i]):
+            c1 = (x - knot_seq[i]) / \
+                 (knot_seq[i + n] - knot_seq[i])
 
-        c2 = (knot_seq[i + n + 1] - x) / \
-             (knot_seq[i + n + 1] - knot_seq[i + 1])
+        if (knot_seq[i + n + 1] - knot_seq[i + 1]):
+            c2 = (knot_seq[i + n + 1] - x) / \
+                 (knot_seq[i + n + 1] - knot_seq[i + 1])
 
         return c1 * N(x, n - 1, i) + c2 * N(x, n - 1, i + 1) 
 
@@ -42,3 +74,21 @@ def get_basis_func(knot_seq, j, n=3, max_cache=16):
         return N(x, n, j)
 
     return basis_func
+
+
+def main():
+    pass
+    
+
+knots = [0, 0, 0, 0, 1, 2, 3, 4, 5, 5, 5]
+index = 1
+
+b = get_basis_func(knots, index)
+
+x = np.linspace(0, 5, 1000)
+b(x)
+plt.plot(x, b(x))
+
+if __name__ == '__main__':
+
+    main()
